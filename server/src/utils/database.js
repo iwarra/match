73f961de75455499d2cd90e9ecf59824f1803d79
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-01-31 23:04:20 +0100

    Path  changed

--- server/src/utils/database.js
+++ server/src/utils/database.js
@@ -1,12 +1,26 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "../../.env" });
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-function CreateDatabase() {
-	const mongoURI = process.env.MONGO_URI;
-	const client = new MongoClient(mongoURI);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// delete appRoot from package, try same with path-module only
+//import appRoot from "app-root-path";
+
+//dotenv.config({ path: path.normalize(appRoot + "/.env") });
+dotenv.config({ path: path.join(__dirname, "../../data/occupation.json") });
+
+let isCreated = false;
+let client;
+
+export function getDatabase() {
+	if (isCreated) return client;
+
+	// path root directory
+	client = new MongoClient(process.env.MONGO_URI);
 	client.connect();
+	isCreated = true;
 	return client;
 }
-
-export const client = CreateDatabase();
