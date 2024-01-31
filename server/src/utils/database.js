4148f09744c6a1b5790f653a35bac1c3a3a753e2
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-01-31 23:49:52 +0100

    env

--- server/src/utils/database.js
+++ server/src/utils/database.js
@@ -1,16 +1,17 @@
-import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import { MongoClient } from "mongodb";
 // delete appRoot from package, try same with path-module only
-//import appRoot from "app-root-path";
 
-//dotenv.config({ path: path.normalize(appRoot + "/.env") });
-dotenv.config({ path: path.join(__dirname, "../../data/occupation.json") });
+if (process.env.ENVIRONMENT !== "prod") {
+	const path = await import("path");
+	const appRoot = await import("app-root-path");
+	console.log("root: ", appRoot.path + "...");
+
+	dotenv.config({ path: path.normalize(appRoot + "/.env") });
+} else {
+	dotenv.config();
+}
+//dotenv.config({ path: path.resolve(__dirname, "..") });
 
 let isCreated = false;
 let client;
